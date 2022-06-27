99b98b5d082a14ad546b725753b39b04c2216f21
ywsCucu <cucutester2@gmail.com>
2022-06-27 18:17:40 +0900

    Unable to find a single main class from the following candidates -> main 주석처리

--- src/main/java/com/care/jenkinsproject/JenkinsprojectGradleApplication.java
+++ src/main/java/com/care/jenkinsproject/JenkinsprojectGradleApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JenkinsprojectGradleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JenkinsprojectGradleApplication.class, args);
-	}
+//	public static void main(String[] args) {
+//		SpringApplication.run(JenkinsprojectGradleApplication.class, args);
+//	}
 
 }
